6c8f6951b345443f59017c0858c323f69cf505a9
Kyle <57875475+caxky@users.noreply.github.com>
2020-01-16 14:51:17 -0500

    Small fix

--- textCreator.h
+++ textCreator.h
@@ -3,7 +3,7 @@
 using namespace sf;
 using namespace std;
 
-Text tex(Font fontStyle, float charSize, int posx, int posy, const Color colorInput, const string stringText){
+Text tex(const Font& fontStyle, float charSize, int posx, int posy, const Color colorInput, const string stringText){
     Text t;
 
     t.setFont(fontStyle);
