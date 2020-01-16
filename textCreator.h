5b28cfa522ef16a89fbbc014e59572ecdc3f5d9b
Kyle <57875475+caxky@users.noreply.github.com>
2020-01-16 14:35:04 -0500

    Complete text creator

--- textCreator.h
+++ textCreator.h
@@ -1,1 +1,16 @@
+#include <SFML/Graphics.hpp>
 
+using namespace sf;
+using namespace std;
+
+Text tex(Font fontStyle, float charSize, int posx, int posy, const Color colorInput, const string stringText){
+    Text t;
+
+    t.setFont(fontStyle);
+    t.setCharacterSize(charSize);
+    t.setPosition(posx, posy);
+    t.setFillColor(colorInput);
+    t.setString(stringText);
+
+    return t;
+}
