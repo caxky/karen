c81df35c608abc12dda400841c838a46a890981c
Kyle <57875475+caxky@users.noreply.github.com>
2020-01-16 14:33:14 -0500

    Removed setTextureRect

--- spriteCreator.h
+++ spriteCreator.h
@@ -3,12 +3,11 @@
 using namespace sf;
 using namespace std;
 
-Sprite playerSprite(Texture playerTexture, float posx, float posy, const IntRect rectint(int x1, int y1, int x2, int y2), float scaleX, float scaleY){
+Sprite playerSprite(Texture playerTexture, float posx, float posy, float scaleX, float scaleY){
     Sprite player;
 
     player.setTexture(playerTexture);
     player.setPosition(posx, posy);
-    player.setTextureRect(rectint);
     player.scale(scaleX, scaleY);
 
     return player;
