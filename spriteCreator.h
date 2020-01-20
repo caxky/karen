84b45fdc1d69c58bfe9f17017d3c05a2bf1f41a2
Kyle <57875475+caxky@users.noreply.github.com>
2020-01-20 12:09:39 -0500

    Small change for sprite function

--- spriteCreator.h
+++ spriteCreator.h
@@ -11,7 +11,7 @@ Texture textr(const string& fileName){
     return t;
 }
 
-Sprite sprite(const Texture& playerTexture, float posx = 0, float posy = 0, float scaleX = 1, float scaleY = 1){
+Sprite sprite(const Texture& playerTexture, float scaleX = 1, float scaleY = 1, float posx = 0, float posy = 0){
     Sprite s;
 
     s.setTexture(playerTexture);
